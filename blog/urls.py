6390afaa1dbe06f909ff1ec504950559daaf42c3
Chandan Mahto <chandanmahto00761@gmail.com>
2019-06-01 10:33:52 +0530

    Update urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -39,13 +39,3 @@ urlpatterns = [
 	path('<path:slug>', user_views.default, name="default"),
 ]
 urlpatterns += staticfiles_urlpatterns()
-
-if settings.DEBUG:
-    import debug_toolbar
-    urlpatterns = [
-        path('__debug__/', include(debug_toolbar.urls)),
-
-        # For django versions before 2.0:
-         #url(r'^__debug__/', include(debug_toolbar.urls)),
-
-    ] + urlpatterns
